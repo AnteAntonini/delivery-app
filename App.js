94ab12ba0aaafa7b71c5bf2ec3f50439b5f8c7f1
AnteAntonini <aantonini@combis.hr>
2023-11-29 20:44:30 +0100

    import preparing order screen into app

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import RestaurantScreen from "./screens/RestaurantScreen";
 import { store } from "./store";
 import { Provider } from "react-redux";
 import BasketScreen from "./screens/BasketScreen";
+import PreparingOrderScreen from "./screens/PreparingOrderScreen";
 
 const Stack = createNativeStackNavigator();
 
